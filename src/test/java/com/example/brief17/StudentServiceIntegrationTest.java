a04f4737a91fbbacdff01ab882defb74f9f44193
VILLAVERDE Olivier <olivier.villaverde@maif.fr>
2025-02-04 10:24:20 +0100

    ajout test integration

--- src/test/java/com/example/brief17/StudentServiceIntegrationTest.java
+++ src/test/java/com/example/brief17/StudentServiceIntegrationTest.java
@@ -14,10 +14,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 
 // TODO: Ajouter les tags nécessaires pour charger H2, charger le profil de test et importer le StudentService
+@DataJpaTest
+@Import(StudentService.class)
+@ActiveProfiles("test")
 class StudentServiceIntegrationTest {
+
+    @Autowired
+    private StudentService studentService;
 
     @Test
     void shouldSaveAndRetrieveStudent() {
         // TODO: Implémenter le test d'intégration, insérer un Student en base de données et le récupérrer
+        Student student = new Student();
+        student.setName("Oli VILLA");
+        student.setAddress("3 rue du moulin blanc");
+
+        Student savedStudent = studentService.saveStudent(student);
+
+        assertThat(savedStudent).isNotNull();
+        assertThat(savedStudent.getId()).isNotNull();
+
+        Optional<Student> retrievedStudent = studentService.findStudentById(savedStudent.getId());
+
+        assertThat(retrievedStudent).isPresent();
+        assertThat(retrievedStudent.get().getName()).isEqualTo("Oli VILLA");
+        assertThat(retrievedStudent.get().getAddress()).isEqualTo("3 rue du moulin blanc");
     }
 }
